42f9a3f1320830f6d9ebddd5fe309cd0baad76c6
Indra.U <indra.ulziibadrakh@example.com>
2023-03-24 17:16:54 +0800

    p.pic uploader fixed, exp-mongo backend united

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,11 +5,47 @@ import usersRouter from "./routers/usersRouter";
 import userRolesRouter from "./routers/userRolesRouter";
 import { loginUser, registerUser } from "./services/usersService";
 
+import multer from "multer";
+import { v4 as uuid } from "uuid";
+import cloudinary from "cloudinary";
+
 const PORT = 8080;
 const app = express();
 
 app.use(json());
 app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "/tmp");
+  },
+  filename: (req, file, cb) => {
+    const fileName = uuid();
+    const splittedPath = file.originalname.split(".");
+    const fileExtension = splittedPath[splittedPath.length - 1];
+    cb(null, `${fileName}.${fileExtension}`);
+  },
+});
+
+cloudinary.config({
+  cloud_name: "df8az1u5k",
+  api_key: "792374388872524",
+  api_secret: "9TvZ89y4BmTTvNM5TY1Gyiasqtc",
+});
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 10 * 1024 * 1024 * 1024,
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.includes("image") || file.mimetype.includes("video")) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  },
+});
 
 app.get("/api/", (req, res) => {
   res.json("Hello world!");
